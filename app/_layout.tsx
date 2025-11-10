6689a09877d58edac3bb5b0f6d62f3667371b286
TableStar <afraroad@gmail.com>
2025-11-10 13:37:06 +0700

    fix: moved index.tsx and added new layout in tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,15 +3,11 @@ import '@/global.css';
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 import { ThemeProvider as NavThemeProvider } from '@react-navigation/native';
-import * as Device from 'expo-device';
-import { Link, Stack } from 'expo-router';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Platform, Pressable } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import { Icon } from '@/components/nativewindui/Icon';
 import { ThemeToggle } from '@/components/nativewindui/ThemeToggle';
-import { cn } from '@/lib/cn';
 import { useColorScheme } from '@/lib/useColorScheme';
 import { NAV_THEME } from '@/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -21,7 +17,7 @@ export {
   ErrorBoundary,
 } from 'expo-router';
 
-const isIos26 = Platform.select({ default: false, ios: Device.osVersion?.startsWith('26.') });
+// const isIos26 = Platform.select({ default: false, ios: Device.osVersion?.startsWith('26.') });
 
 const queryClient = new QueryClient();
 
@@ -41,7 +37,8 @@ export default function RootLayout() {
           <ActionSheetProvider>
             <NavThemeProvider value={NAV_THEME[colorScheme]}>
               <Stack>
-                <Stack.Screen name="index" options={INDEX_OPTIONS} />
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                {/* <Stack.Screen name="index" options={INDEX_OPTIONS} /> */}
                 <Stack.Screen name="modal" options={MODAL_OPTIONS} />
               </Stack>
             </NavThemeProvider>
@@ -53,22 +50,22 @@ export default function RootLayout() {
   );
 }
 
-const INDEX_OPTIONS = {
-  headerLargeTitle: true,
-  headerTransparent: isIos26,
-  title: 'NativewindUI',
-  headerRight: () => <SettingsIcon />,
-} as const;
+// const INDEX_OPTIONS = {
+//   headerLargeTitle: true,
+//   headerTransparent: isIos26,
+//   title: 'NativewindUI',
+//   headerRight: () => <SettingsIcon />,
+// } as const;
 
-function SettingsIcon() {
-  return (
-    <Link href="/modal" asChild>
-      <Pressable className={cn('opacity-80 active:opacity-50', isIos26 && 'px-1.5')}>
-        <Icon name="gearshape" className="text-foreground" />
-      </Pressable>
-    </Link>
-  );
-}
+// function SettingsIcon() {
+//   return (
+//     <Link href="/modal" asChild>
+//       <Pressable className={cn('opacity-80 active:opacity-50', isIos26 && 'px-1.5')}>
+//         <Icon name="gearshape" className="text-foreground" />
+//       </Pressable>
+//     </Link>
+//   );
+// }
 
 const MODAL_OPTIONS = {
   presentation: 'modal',
